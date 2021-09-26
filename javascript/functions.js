5236189ff236d0fdfd72dcc4fe100cc9615fb202
Adrian Nicolalde <adriannicolalde@MacBook-Air-de-Adrian.local>
2021-09-26 22:13:26 +0200

    quite el menu y puse otra pagina y tambien puse un Home y un About+detalles

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,11 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
